05d050bcc50789a6a01a5898db6a94f19fa52ed2
Hatim Muhammed <hatimmohammed369@gmail.com>
2022-01-11 21:07:48 +0200

    Added a bit more docs

--- node.py
+++ node.py
@@ -2,7 +2,6 @@
 # Each node has 5 attributes:
 # parent:
 # type: node
-# default: None
 # 
 # value:
 # type: Any
@@ -16,4 +15,20 @@
 # id:
 # a string basically indicating if this node is left node (id='L')
 # or a right node (id='R')
+# default: '', in case this node is the root
 # 
+# * EXCEPT FOR (id), ALL ATTRIBUTES HAVE (NONE) AS THEIR DEFAULT
+#
+
+
+class node:
+    """
+    the actual code, documentation above
+    """
+    
+    def __init__(self, parent=None, value=None, left=None, right=None, id='') -> None:
+        self.parent: node = parent
+        self.value = value
+        self.left: node = left
+        self.right: node = right
+        self.id: str = id
