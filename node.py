9875c2020c44f978cd1f7c790797215dcc9ee41c
Hatim Muhammed <hatimmohammed369@gmail.com>
2022-01-11 20:54:13 +0200

    Created file (node.py) and added documentation

---
+++ node.py
@@ -0,0 +1,19 @@
+# A class represeting nodes in out tree
+# Each node has 5 attributes:
+# parent:
+# type: node
+# default: None
+# 
+# value:
+# type: Any
+#
+# left node:
+# type is clear right from name
+# 
+# right node:
+# so does this one
+# 
+# id:
+# a string basically indicating if this node is left node (id='L')
+# or a right node (id='R')
+# 
