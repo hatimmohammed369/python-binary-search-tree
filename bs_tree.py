497d761b06d7aab41c0ced45ab79a4d16dd3dbf0
Hatim Muhammed <hatimmohammed369@gmail.com>
2022-01-11 22:04:36 +0200

    Added documentation to bs_tree.py

---
+++ bs_tree.py
@@ -0,0 +1,18 @@
+# class bs_tree (binary search tree)
+#   duplicates are allowed
+#
+# attributes:
+#   root, size, get_node, insert, remove
+# 
+# root:
+#   default: node(parent=None, value=None, left=None, right=None, id='')
+#   represents tree root
+#
+# size:
+#   default: 0
+#   how many "real-life tree leafs" are there, simply put, how many elements in out tree
+#
+# get_node:
+#   method
+#   given an item, like 4, returns the first node(n) with attribute 
+
